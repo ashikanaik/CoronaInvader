a2e4f614580e9c63d205153601486309d375ba5f
Ashika K Naik <48836337+ashikanaik@users.noreply.github.com>
2021-09-22 19:12:09 +0530

    initial commit

--- Game.py
+++ Game.py
@@ -16,6 +16,8 @@ mixer.music.play(-1)
 
 pygame.display.set_caption("Corona Invader")
 
+icon = pygame.image.load('coronavirus.png')
+pygame.display.set_icon(icon)
 
 player_image = pygame.image.load('VaccineResized.png')
 playerX = 600
@@ -27,11 +29,11 @@ enemyX = []
 enemyY = []
 enemyX_change = []
 enemyY_change = []
-enemies_num = 5
+enemies_num = 7
 
 for i in range(enemies_num):
     enemy_image.append(pygame.image.load('CoronavirusResized.png'))
-    enemyX.append(random.randint(0, 1200))
+    enemyX.append(random.randint(0, 1225))
     enemyY.append(random.randint(45, 140))
     enemyX_change.append(4)
     enemyY_change.append(40)
@@ -51,7 +53,7 @@ font = pygame.font.Font('freesansbold.ttf', 35)
 textX = 10
 textY = 10
 
-end_font = pygame.font.Font('freesansbold.ttf', 64)
+end_font = pygame.font.Font('Organic Brand.ttf', 64)
 
 
 def show_points(x, y):
@@ -59,7 +61,7 @@ def show_points(x, y):
     screen.blit(score, (x, y))
 
 def game_over_message():
-    over_text = end_font.render("CORONA WON", True, (254, 0, 246))
+    over_text = end_font.render("£ corona won £", True, (254, 0, 246))
     screen.blit(over_text, (400, 300))
 
 def player(x, y):
@@ -109,8 +111,8 @@ while running:
     playerX += playerX_change
     if playerX <= 0:
         playerX = 0
-    elif playerX >= 1201:
-        playerX = 1201
+    elif playerX >= 1225:
+        playerX = 1225
 
 
     for i in range(enemies_num):
@@ -123,10 +125,10 @@ while running:
 
         enemyX[i] += enemyX_change[i]
         if enemyX[i] <= 0:
-            enemyX_change[i] = 4
+            enemyX_change[i] = 2
             enemyY[i] += enemyY_change[i]
-        elif enemyX[i] >= 1200:
-            enemyX_change[i] =-4
+        elif enemyX[i] >= 1225:
+            enemyX_change[i] =-2
             enemyY[i] += enemyY_change[i]
 
 
@@ -137,13 +139,13 @@ while running:
             bulletY = 590
             bullet_state = "ready"
             score_value += 1
-            enemyX[i] = random.randint(0, 1200)
+            enemyX[i] = random.randint(0, 1225)
             enemyY[i] = random.randint(50, 150)
 
         enemy(enemyX[i], enemyY[i], i)
 
     if bulletY <= 0:
-        bulletY = 590
+        bulletY = 585
         bullet_state = "ready"
 
     if bullet_state is "fire":
